d598e1e3e2f2c15bb3de1f20544e73a0b35c6310
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-06-08 22:50:54 -0300

    Start project

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Ezequiel Rasgido on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
