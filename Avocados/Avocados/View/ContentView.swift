1d835ba6c008fa9b4abee7ce276d63f4525d29d4
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-06-14 00:58:15 -0300

    Add headers

--- Avocados/Avocados/View/ContentView.swift
+++ Avocados/Avocados/View/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    var headers = headersData
+    
     var body: some View {
-        Text("Avocado Recipes")
-            .padding()
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack(alignment: .center, spacing: 20) {
+                // MARK: - HEADER
+                
+                ScrollView(.horizontal, showsIndicators: true) {
+                    HStack(alignment: .top, spacing: 0) {
+                        ForEach(headers) { item in
+                            HeaderView(header: item)
+                        }
+                    }
+                }
+                
+                // MARK: - FOOTER
+                
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All about avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you wanted to know about avocados but were to afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(headers: headersData)
     }
 }
